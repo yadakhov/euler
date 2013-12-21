cf9ab3962f71d2a1f569d838ccde8b0a755194fd
Yada Khov <yada.khov@gmail.com>
2013-12-21 09:53:37 -0500

    Add problem 9

---
+++ src/com/yadakhov/euler/Euler9.java
@@ -0,0 +1,16 @@
+package com.yadakhov.euler;
+
+/**
+ * Special Pythagorean triplet
+ * Problem 9
+ */
+public class Euler9 {
+    public static void main(String[] args) {
+        // compute all 3 numbers that add to 1000
+        int a = 1;
+        int b = 2;
+        int c = 1000 - b - a;
+
+
+    }
+}
